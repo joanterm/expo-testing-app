bfb487804969fb295d543cb247a09f748a7619df
joanterm <jo_joanarg@yahoo.com>
2023-07-20 15:19:44 -0400

    Added a modal to the home screen

--- components/Home.js
+++ components/Home.js
@@ -1,10 +1,27 @@
-import { StyleSheet, Text, View, TouchableOpacity} from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity, Modal} from 'react-native'
+import { useState } from 'react'
 import { globalStyles } from '../styles/globalStyles'
 import { Image } from 'expo-image'
+import { Entypo } from '@expo/vector-icons'
 
 const Home = ({navigation}) => {
+    const [isModalOpen, setIsModalOpen] = useState(false)
+
     return ( 
         <View style={styles.homeContainer}>
+            {/* MODAL HERE, ONLY VISIBLE WHEN TOGGLED*/}
+            <Modal visible={isModalOpen}>
+                <View style={styles.modalContainer}>
+                    <Entypo name="circle-with-cross" style={styles.modalIcon} size={40} color="#92FBEB" onPress={() => setIsModalOpen(false)}/>
+                    <Text>MODAL TEXT HERES</Text>
+                </View>
+            </Modal>
+            {/* MODAL ENDS */}
+            <View style={styles.infoIconOuterContainer}>
+                <View style={styles.infoIconInnerContainer}>
+                    <Entypo name="info" size={24} color="#ffff" onPress={() => setIsModalOpen(true)}/>
+                </View>
+            </View>
             <Image 
                 style={styles.homeImage} 
                 source={require("../assets/main-image.png")}
@@ -14,7 +31,7 @@ const Home = ({navigation}) => {
             <TouchableOpacity 
                 style={globalStyles.buttonStyle} 
                 onPress={() => navigation.navigate('ContactsContainer')}>
-                <Text style={globalStyles.textStyle}>Go to contacts</Text>
+                <Text style={globalStyles.textStyle}>GO TO CONTACTS</Text>
             </TouchableOpacity>
         </View>
      )
@@ -35,6 +52,22 @@ const styles = StyleSheet.create({
         fontSize: 20,
         marginVertical: 20,
         textAlign: "center"
+    },
+    infoIconOuterContainer: {
+        alignItems: 'flex-end'
+    },
+    infoIconInnerContainer: {
+        alignItems: "center",
+        padding: 6,
+        borderRadius: 50,
+        backgroundColor: "#92FBEB"
+    },
+    modalContainer: {
+        flex: 1,
+        padding: 40
+    },
+    modalIcon: {
+        textAlign: "right"
     }
 })
  
